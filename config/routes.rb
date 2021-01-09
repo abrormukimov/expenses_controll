1c1c201a2cf907f5bb28d0a9388957d164ec9565
abrormukimov <abrormm34@gmail.com>
2021-01-09 20:12:35 +0500

    Add multiple routes and some helpful gems

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' } do
 		get ':user/edit-profile' => 'devise/registration#edit', as: :edit_user_profile
 	end	
-  
+   
 	resources :expenses
 	resources :groups
 	resources :group_expenses, only: [:create, :destroy]
